a2d4baffb092e2a419cf9e3f23266a0add535f6b
Ayush <iamayush02@gmail.com>
2024-09-07 01:23:12 +0530

    Made changes in Wallet Screen

--- client/src/screens/Account.js
+++ client/src/screens/Account.js
@@ -1,11 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Account = () => {
   return (
-    <View>
-      <Text>Account</Text>
-    </View>
+    <SafeAreaView style={{flex:1}}>
+      {/* Header */}
+      <View>
+
+      </View>
+      {/* profile Picture */}
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <View style={{width:200,height:200,backgroundColor:'#00704A',borderRadius:126}}>
+        <View style={{width:180,height:180,backgroundColor:'lightgrey',borderRadius:110,margin:10}}>
+
+        </View>
+      </View>
+
+      </View>
+    </SafeAreaView>
   )
 }
 
--- client/src/screens/Wallet.js
+++ client/src/screens/Wallet.js
@@ -1,14 +1,105 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+import IonIcons from 'react-native-vector-icons/Ionicons';
 
 const Wallet = () => {
   return (
-    <View>
-      <Text>Wallet</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{flex: 1, margin: 20}}>
+      {/* header */}
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        {/* left side text */}
+        <View>
+          <Text style={{fontSize: 20, fontWeight: 700}}>Wallet</Text>
+          <Text>Low Balance</Text>
+        </View>
+        {/* right side cart */}
+        <View>
+          <IonIcons name="cart" size={30} />
+        </View>
+      </View>
 
-export default Wallet
+      {/* card image */}
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          // bottom: 100,
+        }}>
+        <View
+          style={{
+            borderWidth: 2,
+            height: '30%',
+            width: '95%',
+            borderRadius: 30,
+          }}>
+          {/* <Text>Balance</Text> */}
+          <ImageBackground
+            source={require('../assets/images/wallet.png')}
+            style={{flex: 1}}
+            resizeMode="cover">
+            <View
+              style={{
+                justifyContent: 'center',
+                flex: 1,
+                alignItems: 'flex-start',
+                margin: 10,
+              }}>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  color: '#fff',
+                  fontWeight: 700,
+                  fontSize: 40,
+                  opacity: 1,
+                }}>
+                Balance
+              </Text>
+            </View>
+          </ImageBackground>
+        </View>
+        <View style={{margin: 20, padding: 20, flexDirection: 'row'}}>
+          <TouchableOpacity
+            style={{
+              height: 65,
+              width: 65,
+              backgroundColor: 'lightgrey',
+              marginRight: 30,
+            }}></TouchableOpacity>
+            <Text></Text>
+          <TouchableOpacity
+            style={{
+              height: 65,
+              width: 65,
+              backgroundColor: 'lightgrey',
+              marginRight: 30,
+            }}></TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              height: 65,
+              width: 65,
+              backgroundColor: 'lightgrey',
+              marginRight: 30,
+            }}></TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              height: 65,
+              width: 65,
+              backgroundColor: 'lightgrey',
+            }}></TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default Wallet;
+
+const styles = StyleSheet.create({});
